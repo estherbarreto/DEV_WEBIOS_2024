413f7391021094d5a828f3ecad7b15c37af97872
estherbarreto <esther.barreto0666@gmail.com>
2024-05-08 21:44:10 -0300

    primeira aula java

---
+++ JS/JS02-Variaveis/main.js
@@ -0,0 +1,41 @@
+console.log("Hello World")
+
+var _felippe = 8
+_felippe = "Messi"
+
+console.log(_felippe)
+
+let letsGo = 4
+letsGo = 6
+console.log(letsGo)
+
+console.clear()
+
+const PI = 3.1415
+
+console.log(PI)
+
+let NAN = 0 / 0
+
+console.log(NAN)
+
+let corinthians = "Melhor time, palmeiras sem mundial"
+
+console.log (corinthians)
+
+let IOS = "Instituto da Oportunidade social"
+
+console.log("Instituto" + "da" + "oportunidade" + "social")
+
+var numero1 =2
+var texto ="Eu tenho"
+var texto2 = "reais"
+
+console.log(texto + numero1 +texto2 )
+
+console.clear()
+
+var num1 = 1
+var num2 = 1
+
+console.log(num1 == num2)
