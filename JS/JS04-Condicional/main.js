94379a6d02d0a3730248f296a4cdf82f40547079
estherbarreto <esther.barreto0666@gmail.com>
2024-05-14 21:40:08 -0300

    else e switch

---
+++ JS/JS04-Condicional/main.js
@@ -0,0 +1,63 @@
+var TodosDeCracha = false
+
+if(TodosDeCracha == true){
+    console.log("Todos recebem nota dez em JS")
+
+}else{
+    console.log("Todos recebem nota zero em JS")
+
+}
+
+console.clear()
+
+// exercicio em sala
+var num1 = 3
+var num2 = 6
+
+// var compara = 3 == 6
+
+// if(compara == true){
+//     console.log("o número 1 é maior que o número 2")
+// }else{
+//     console.log("o número 1 é menor que o número 2")
+// }
+
+let mes = "Outubro"
+
+if(mes == "JANEIRO"){
+    console.log("é verão")
+}else if(mes == "JUNHO"){
+    console.log("é outono")
+}else if(mes == "Outubro"){
+    console.log("é primavera")
+}else{
+    console.log("é inverno")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.clear()
+
+// console.log("rafael eu te amo mas vc é viadinho")
+// console.log("queria te avisar que")
+// console.log("vamos FALTAR amanhã")
+// console.log("<3")
+
+// console.log("oi gabriel </3")
+// console.log("oi polly <3")
+
+// console.log( "rafaellinha amo você desculpemezinho por hoje")
+// console.log ("<3")
