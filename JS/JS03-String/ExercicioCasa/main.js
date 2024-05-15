14afb7066976cdc7195a0e0e112380ae8d1c3b57
estherbarreto <esther.barreto0666@gmail.com>
2024-05-15 19:39:15 -0300

    exercicio casa java

---
+++ JS/JS03-String/ExercicioCasa/main.js
@@ -0,0 +1,6 @@
+var texto = "Estamos aprendendo JavaScript e seremos futuros devs"
+
+console.log(texto.charAt(19))
+console.log(`na variavel texto tem ${texto.length} caracteres`)
+console.log(`${texto.toUpperCase()}`)
+console.log(texto.substring(48, 52))
