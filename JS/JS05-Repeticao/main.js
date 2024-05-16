1a51b8f772f9099f600547d9a5313a2719e61623
estherbarreto <esther.barreto0666@gmail.com>
2024-05-15 21:41:27 -0300

    aula 05 java

---
+++ JS/JS05-Repeticao/main.js
@@ -0,0 +1,20 @@
+var contador = 0
+
+
+while(contador < 10){
+    console.log(contador + " ainda é menor que 10")
+    debugger
+    contador ++
+
+}
+
+var contador  = 0
+do{
+    debugger
+   console.log(contador + " ainda é menor que 10")
+  contador++
+}while(contador < 10)
+
+    for(var contador = 10; contador < 10; contador++){
+        console.log(contador + " ainda é menor que 10")   
+    }
