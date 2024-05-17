0ba30f86d48bca9e38c54b16c222f472bc66e09b
estherbarreto <esther.barreto0666@gmail.com>
2024-05-17 19:18:27 -0300

    exercicio casa js5

---
+++ JS/JS05-Repeticao/ExercicioCasa/main.js
@@ -0,0 +1,9 @@
+var tabuada = 3
+
+if (tabuada === 2 || tabuada === 3) {
+    console.log(`Tabuada do número ${tabuada} :`)
+
+    for(var numero = 1 ; numero <= 10 ; numero++) {
+        console.log(`${tabuada} x ${numero} = ${tabuada * numero}`)
+    }
+}
