a77be9283172781ddd41c84e2c33ab4a0840f8c4
estherbarreto <esther.barreto0666@gmail.com>
2024-05-24 19:59:10 -0300

    tentativa

---
+++ JS/JS09-MetodoArray/main.js
@@ -0,0 +1,7 @@
+let frutas = ['morango, amora, melancia, maçã, limão, manga']
+
+frutas.forEach(retornaInfo);
+function retornaInfo(nomeFruta, indice){
+    console.log(`nomeFruta: ${nomeFruta}`);
+    console.log(`indice: ${indice}`);
+}
